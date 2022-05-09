78dec7d076bc44a4d785cc689429a135f23fe9a9
allexib <ooomegasib@gmail.com>
2022-05-09 17:06:38 +0300

    product,footer

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,11 @@ const Home = ({products, bannerData}) => {
                 <p>Speakers of many variations</p>
             </div>
 
-            <div className='product-container'>
-                {products?.map((product) => product.name)}
+            <div className='products-container'>
+                {products?.map((product) => <Product key={product._id} product={product}/>)}
             </div>
 
-            <FooterBanner/>
+            <FooterBanner footerBanner={bannerData && bannerData[0]}/>
         </>
     )
 }
