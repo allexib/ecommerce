f65bd072ab539787c560f6a3e16d12e91cc7e6ba
allexib <ooomegasib@gmail.com>
2022-05-10 18:44:18 +0300

    product deatails,pages of empty product

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ const Home = ({products, bannerData}) => {
 
             <div className='products-heading'>
                 <h2>Best selling products</h2>
-                <p>Speakers of many variations</p>
+                <p>Watch of many variations</p>
             </div>
 
             <div className='products-container'>
